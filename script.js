a1975a50c16eea9edd71d2c26099089eb5e6e194
ukates <derrickcates@icloud.com>
2021-04-28 17:30:13 -0400

    Adding comments so future me remembers how I did this

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//function to generate random number accepts num as param
 const generateRandomNumber = num => Math.floor(Math.random() * num); 
-
+//three player team object has 3 properties with array of top ten players in each category
 const threePlayerTeam = {
     pg: ["Luka Doncic", "Damian Lillard", "Stephen Curry", "Ben Simmons", "Russell Westbrook",
         "kyrie Irving", "Jamal Murray", "Ja Morant", "Trae Young", "De'Aaron Fox"], 
@@ -8,7 +9,7 @@ const threePlayerTeam = {
     center: ["Nikola Jokic", "Joel Embiid", "Karl Anthony Towns", "Rudy Gobert", "Bam Adebayo",
         "Kristaps Porzingis", "Jusuf Nurkic", "Nikola Vucevic", "DeAndre Ayton", "Jonas Valanciunas"]
 }; 
-
+//accepts a object of arrays, loops through each prop and selects random player from array, pushes to team array
 const generateTeam = teamObject => {
     let team = []
     for(let property in teamObject) {
@@ -16,5 +17,5 @@ const generateTeam = teamObject => {
     }
     return team;  
 }
-
+//display team array for testing purposes
 console.log(generateTeam(threePlayerTeam)); 
