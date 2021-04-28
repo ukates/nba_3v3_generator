2d1c167a49dccfc64ece4464147b102e5cf52e11
ukates <derrickcates@icloud.com>
2021-04-28 17:50:12 -0400

    Adding function to display team, and console log to make it look nice

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const generateRandomNumber = num => Math.floor(Math.random() * num);
 //three player team object has 3 properties with array of top ten players in each category
 const threePlayerTeam = {
     pg: ["Luka Doncic", "Damian Lillard", "Stephen Curry", "Ben Simmons", "Russell Westbrook",
-        "kyrie Irving", "Jamal Murray", "Ja Morant", "Trae Young", "De'Aaron Fox"], 
+        "Kyrie Irving", "Jamal Murray", "Ja Morant", "Trae Young", "De'Aaron Fox"], 
     sf: ["LeBron James", "Kawhi Leonard", "Jimmy Butler", "Khris Middleton", "Jayson Tatum",
         "Brandon Ingram", "Gordon Hayward", "DeMar DeRozan", "Will Barton", "Bojan Bogdanovic"],
     center: ["Nikola Jokic", "Joel Embiid", "Karl Anthony Towns", "Rudy Gobert", "Bam Adebayo",
@@ -18,4 +18,10 @@ const generateTeam = teamObject => {
     return team;  
 }
 //display team array for testing purposes
-console.log(generateTeam(threePlayerTeam)); 
+const displayTeam = team => {console.log(`\nPoint Guard   : ${team[0]}\nSmall Forward : ${team[1]}\nCenter        : ${team[2]}`)};
+//give user console information about app and advise to see team below
+console.log("\n\n*****NBA 3V3 TEAM GENERATOR*****\n"); 
+console.log("Check below for your random team!\n"); 
+console.log("           ↓↓↓↓↓↓↓↓↓↓");
+//runs displayteam function which inputs the generate team function to get its return value
+displayTeam(generateTeam(threePlayerTeam)); 
